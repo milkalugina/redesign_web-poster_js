e793ed74be66d4b0f411e899a0a611325dc8891b
Mila Kalugina <milkalugin@gmail.com>
2020-06-05 00:06:52 +0300

    добавила последнюю страницу

---
+++ js/result.js
@@ -0,0 +1,17 @@
+document.getElementById('rect_1').onclick = function() {
+  document.getElementById('rect_1').style.opacity = '0%';
+  document.getElementById('rect_1').style.zIndex = '2';
+  document.getElementById('first_p').style.color = '#FF7B7B';
+}
+
+document.getElementById('rect_2').onclick = function() {
+  document.getElementById('rect_2').style.opacity = '0%';
+  document.getElementById('rect_2').style.zIndex = '2';
+  document.getElementById('second_p').style.color = '#FF7B7B';
+}
+
+document.getElementById('rect_3').onclick = function() {
+  document.getElementById('rect_3').style.opacity = '0%';
+  document.getElementById('rect_3').style.zIndex = '2';
+  document.getElementById('third_p').style.color = '#FF7B7B';
+}
