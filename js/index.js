b148964d53d15805934896c319a67d3d16835641
Mila Kalugina <milkalugin@gmail.com>
2020-06-05 12:31:45 +0300

    доработала анимацию на послденей странице

--- js/index.js
+++ js/index.js
@@ -1,35 +1,20 @@
 
 let controller = new ScrollMagic.Controller()
 
-let sections = document.querySelectorAll('section')
-
-let count = 1
-while (count < sections.length) {
-new ScrollMagic.Scene({
-  triggerHook: 'onLeave',
-  triggerElement: sections[count]
-})
-.addTo(controller)
-.setPin(sections[count])
-count += 1
-}
-
-
- // cats_scroll
 let $catsScroll = document.getElementById('main_cat')
 
 let catsScrollAnim = anime({
-targets: $catsScroll,
-scale: 20,
-autoplay: false,
-easing: 'easeInOutCirc'
+  targets: $catsScroll,
+  scale: 20,
+  autoplay: false,
+  easing: 'easeInOutCirc'
 })
 
 let $section1 = document.getElementById('cats_scroll')
 
 new ScrollMagic.Scene({
-triggerElement: '#cats_scroll',
-duration: $section1.getBoundingClientRect().height
+  triggerElement: '#cats_scroll',
+  duration: $section1.getBoundingClientRect().height
 })
 .addTo(controller)
 .setPin($catsScroll)
--- js/result.js
+++ js/result.js
@@ -15,3 +15,114 @@ document.getElementById('rect_3').onclick = function() {
   document.getElementById('rect_3').style.zIndex = '2';
   document.getElementById('third_p').style.color = '#FF7B7B';
 }
+
+// sketches
+document.getElementById('sketch_1').onclick = function() {
+  document.getElementById('sketch_1-01').style.opacity = '100%';
+  document.getElementById('sketch_1-02').style.opacity = '100%';
+  document.getElementById('sketch_1-03').style.opacity = '100%';
+}
+
+document.getElementById('sketch_2').onclick = function() {
+  document.getElementById('sketch_2-01').style.opacity = '100%';
+  document.getElementById('sketch_2-02').style.opacity = '100%';
+  document.getElementById('sketch_2-03').style.opacity = '100%';
+}
+
+document.getElementById('sketch_3').onclick = function() {
+  document.getElementById('sketch_3-01').style.opacity = '100%';
+  document.getElementById('sketch_3-02').style.opacity = '100%';
+  document.getElementById('sketch_3-03').style.opacity = '100%';
+}
+
+document.getElementById('sketch_4').onclick = function() {
+  document.getElementById('sketch_4-01').style.opacity = '100%';
+  document.getElementById('sketch_4-02').style.opacity = '100%';
+  document.getElementById('sketch_4-03').style.opacity = '100%';
+}
+
+document.getElementById('sketch_5').onclick = function() {
+  document.getElementById('sketch_5-01').style.opacity = '100%';
+  document.getElementById('sketch_5-02').style.opacity = '100%';
+  document.getElementById('sketch_5-03').style.opacity = '100%';
+}
+
+document.getElementById('sketch_6').onclick = function() {
+  document.getElementById('sketch_6-01').style.opacity = '100%';
+  document.getElementById('sketch_6-02').style.opacity = '100%';
+  document.getElementById('sketch_6-03').style.opacity = '100%';
+}
+
+//animations
+
+document.getElementById('sketch_1-01').onclick = function() {
+  document.getElementById('sketch_1-01').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_1-02').onclick = function() {
+  document.getElementById('sketch_1-02').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_1-03').onclick = function() {
+  document.getElementById('sketch_1-03').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_2-01').onclick = function() {
+  document.getElementById('sketch_2-01').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_2-02').onclick = function() {
+  document.getElementById('sketch_2-02').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_2-03').onclick = function() {
+  document.getElementById('sketch_2-03').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_3-01').onclick = function() {
+  document.getElementById('sketch_3-01').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_3-02').onclick = function() {
+  document.getElementById('sketch_3-02').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_3-03').onclick = function() {
+  document.getElementById('sketch_3-03').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_4-01').onclick = function() {
+  document.getElementById('sketch_4-01').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_4-02').onclick = function() {
+  document.getElementById('sketch_4-02').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_4-03').onclick = function() {
+  document.getElementById('sketch_4-03').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_5-01').onclick = function() {
+  document.getElementById('sketch_5-01').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_5-02').onclick = function() {
+  document.getElementById('sketch_5-02').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_5-03').onclick = function() {
+  document.getElementById('sketch_5-03').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_6-01').onclick = function() {
+  document.getElementById('sketch_6-01').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_6-02').onclick = function() {
+  document.getElementById('sketch_6-02').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
+
+document.getElementById('sketch_6-03').onclick = function() {
+  document.getElementById('sketch_6-03').style.transform =  `translate(${anime.random(-55,55)}vw,${anime.random(-55,55)}vh) rotate(${anime.random(0, 360)}deg)`
+}
