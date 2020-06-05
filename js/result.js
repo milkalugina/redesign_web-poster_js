bfa086bea23269cf981bb29c16682e6f60db10e0
Mila Kalugina <milkalugin@gmail.com>
2020-06-05 13:07:00 +0300

    добавил анимацию основным скетчам

--- js/result.js
+++ js/result.js
@@ -21,36 +21,42 @@ document.getElementById('sketch_1').onclick = function() {
   document.getElementById('sketch_1-01').style.opacity = '100%';
   document.getElementById('sketch_1-02').style.opacity = '100%';
   document.getElementById('sketch_1-03').style.opacity = '100%';
+  document.getElementById('sketch_1').style.animation = '1s linear 0s normal none infinite rot';
 }
 
 document.getElementById('sketch_2').onclick = function() {
   document.getElementById('sketch_2-01').style.opacity = '100%';
   document.getElementById('sketch_2-02').style.opacity = '100%';
   document.getElementById('sketch_2-03').style.opacity = '100%';
+  document.getElementById('sketch_2').style.animation = '1s linear 0s normal none infinite rot';
 }
 
 document.getElementById('sketch_3').onclick = function() {
   document.getElementById('sketch_3-01').style.opacity = '100%';
   document.getElementById('sketch_3-02').style.opacity = '100%';
   document.getElementById('sketch_3-03').style.opacity = '100%';
+  document.getElementById('sketch_3').style.animation = '1s linear 0s normal none infinite rot';
 }
 
 document.getElementById('sketch_4').onclick = function() {
   document.getElementById('sketch_4-01').style.opacity = '100%';
   document.getElementById('sketch_4-02').style.opacity = '100%';
   document.getElementById('sketch_4-03').style.opacity = '100%';
+  document.getElementById('sketch_4').style.animation = '1s linear 0s normal none infinite rot';
 }
 
 document.getElementById('sketch_5').onclick = function() {
   document.getElementById('sketch_5-01').style.opacity = '100%';
   document.getElementById('sketch_5-02').style.opacity = '100%';
   document.getElementById('sketch_5-03').style.opacity = '100%';
+  document.getElementById('sketch_5').style.animation = '1s linear 0s normal none infinite rot';
 }
 
 document.getElementById('sketch_6').onclick = function() {
   document.getElementById('sketch_6-01').style.opacity = '100%';
   document.getElementById('sketch_6-02').style.opacity = '100%';
   document.getElementById('sketch_6-03').style.opacity = '100%';
+  document.getElementById('sketch_6').style.animation = '1s linear 0s normal none infinite rot';
 }
 
 //animations
